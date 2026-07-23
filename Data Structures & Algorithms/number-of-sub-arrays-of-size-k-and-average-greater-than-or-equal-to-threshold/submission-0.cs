b223df61ecf7fd246b76b4a3defa5794284ce850
ciasma <marcia.small@gmail.com>
2026-07-23 14:18:03 -0400

    Add: number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold - submission-0

---
+++ Data Structures & Algorithms/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/submission-0.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int NumOfSubarrays(int[] arr, int k, int threshold) {
+
+        int count = 0, total = 0, j = 0;
+        for(var i = 0; i < arr.Length; i ++){
+            while(j-i < k  && (i + k <= arr.Length)){
+                total += arr[j];
+                
+                j++;
+            }
+            Console.WriteLine(total);
+
+            if(total/k >= threshold)
+            {
+                count++;
+                 
+            }
+            j = i + 1;
+            total = 0;
+        }
+        return count;
+        
+    }
+}
